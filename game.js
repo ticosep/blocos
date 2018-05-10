10010abd7b439190ffabaa40dd3fcc6610299c89
ticosep <ticoparronchi@gmail.com>
2018-05-10 01:51:03 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,31 +1,9 @@
-var tela = new PIXI.Application(800, 600);
-document.body.appendChild(tela.view);
-
-
-
-//exemplo fucnional --- melhorar para possivel js file
-
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'levels.json', true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-
-            // .open will NOT return a value but simply returns undefined in async mode so use a callback
-            callback(xobj.responseText);
-
-        }
-    }
-    xobj.send(null);
-
-}
-
+//funcao para carregar json em variavel
 
 loadJSON(function(response) {
-    
- var level = JSON.parse(response);
- alert(level.levels[1].name);
-});
+   
+     level = JSON.parse(response);
+     alert(level.levels[1].name);
 
+    
+});
