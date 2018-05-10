4497222f91dd5bd36a303dd196589c729f0c9c09
ticosep <ticoparronchi@gmail.com>
2018-05-10 00:58:35 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,31 @@
 var tela = new PIXI.Application(800, 600);
 document.body.appendChild(tela.view);
 
-const fs = require('./levels.json');
+
+
+
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'levels.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+
+        }
+    }
+    xobj.send(null);
+
+}
+
+
+loadJSON(function(response) {
+    
+ var level = JSON.parse(response);
+ alert(level.name);
+});
+
+
