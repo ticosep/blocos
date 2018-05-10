359ebd202c7a378d7fc237195abf04172b92eeff
ticosep <ticoparronchi@gmail.com>
2018-05-10 01:50:42 -0300

    Create carregaJSON.js

---
+++ carregaJSON.js
@@ -0,0 +1,18 @@
+
+
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'levels.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+
+        }
+    }
+    xobj.send(null);
+
+}
