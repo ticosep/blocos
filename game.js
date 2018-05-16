ae485426a00356fe99ccefa4dd75aaba361b5d68
ticosep <ticoparronchi@gmail.com>
2018-05-16 03:22:00 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 var levelAtual = 0;
-var tela, trilho, bloco1, bloco2;
+var tela, trilho, bloco1, bloco2, mod1, mod2;
 var modif = [];
 
 
-colisao = new Bump(PIXI);
-
+var colisao = new Bump(PIXI);
 setup();
 
 function setup() {
@@ -17,15 +16,17 @@ function setup() {
   
 function gameLoop(delta){
   //Update the current game state:
-  alert(bloco1);
-  state(delta);
+
+state(delta);
 }
+
 function play(delta) {
   
   if (colisao.hit(bloco1,bloco2)) {
 
-    alert('a');
-    
+    console.log('mudaLevel');
+    levelAtual = levelAtual + 1;
+    setup();
 
   } else {
 
@@ -35,8 +36,11 @@ function play(delta) {
   }
 }
 
+
+
 function carregaLevelAtual(levelAtual){
 
+   
     geraTela();
     geraBlocos(levelAtual);
     criaMod(lvls.levels[levelAtual].modifiers.length);
@@ -58,77 +62,110 @@ function geraTela(){
     trilho.moveTo(0,window.innerHeight/2);
     trilho.lineTo(window.innerWidth,window.innerHeight/2);
     tela.stage.addChild(trilho);
+    
 }
 
 function geraBlocos(levelAtual){
     //gera e posiciona blocos do jogo de acordo com a janela do navegador
-   bloco1 = new PIXI.Graphics();
-    bloco2 = new PIXI.Graphics();
+    var blocoGraph2 = new PIXI.Graphics();
+    var blocoGraph = new PIXI.Graphics();
 
-   
-    
     if (lvls.levels[levelAtual].initial[0] == 1){
-        bloco1.beginFill(parseInt(lvls.levels[levelAtual].initial[1],16));
-        bloco1.drawRect(0,window.innerHeight/2 - (window.innerHeight/6)/2, window.innerHeight/6, window.innerHeight/6);
-        bloco1.endFill();
+        
+
+        blocoGraph.beginFill(parseInt(lvls.levels[levelAtual].initial[1],16));
+        blocoGraph.drawRect(0,0, window.innerHeight/6, window.innerHeight/6);
+        blocoGraph.endFill();
+        //transforma o retangulo criado em um sprite
+        bloco1 = new PIXI.Sprite(blocoGraph.generateCanvasTexture());
+        bloco1.x = 0;
+        bloco1.y =  window.innerHeight/2 - window.innerHeight/12;
+        //deixa bloco interativo
+        bloco1.interactive = true;
+        bloco1.buttonMode = true;
+        bloco1.on('click',iniciaCaminho);
+        tela.stage.addChild(bloco1);
+        
+    }
+    else {
+        
+        
+        blocoGraph.beginFill(parseInt(lvls.levels[levelAtual].initial[1],16));
+        blocoGraph.drawRect(0,0, window.innerHeight/6, window.innerHeight/4);
+        blocoGraph.endFill();
+        //transforma o retangulo criado em um sprite
+        bloco1 = new PIXI.Sprite(blocoGraph.generateCanvasTexture());
+        bloco1.x = 0;
+        bloco1.y =  window.innerHeight/2 - window.innerHeight/8;
         //deixa bloco interativo
         bloco1.interactive = true;
         bloco1.buttonMode = true;
         bloco1.on('click',iniciaCaminho);
         tela.stage.addChild(bloco1);
     }
-    else {
-        bloco1.beginFill(parseInt(lvls.levels[levelAtual].initial[1],16));
-        bloco1.drawRect(0,window.innerHeight/2 - (window.innerHeight/4)/2, window.innerHeight/6, window.innerHeight/4);
-        bloco1.endFill();
-        bloco1.interactive = true;
-        bloco1.buttonMode = true;
-        bloco1.on('click',iniciaCaminho);
-        tela.stage.addChild(bloco1);
-    }
     if (lvls.levels[levelAtual].final[0] == 1){
-        bloco2.beginFill(parseInt(lvls.levels[levelAtual].final[1],16));
-        bloco2.drawRect(window.innerWidth - window.innerHeight/6 ,window.innerHeight/2 - (window.innerHeight/6)/2, window.innerHeight/6, window.innerHeight/6);
-        bloco2.endFill();
+        
+        
+        blocoGraph2.beginFill(parseInt(lvls.levels[levelAtual].final[1],16));
+        blocoGraph2.drawRect(0 ,0, window.innerHeight/6, window.innerHeight/6);
+        blocoGraph2.endFill();
+
+        bloco2 = new PIXI.Sprite(blocoGraph2.generateCanvasTexture());
+        bloco2.x = window.innerWidth - window.innerHeight/6;
+        bloco2.y = window.innerHeight/2 - window.innerHeight/12;
         tela.stage.addChild(bloco2);
     }
     else {
-        bloco2.beginFill(parseInt(lvls.levels[levelAtual].final[1],16));
-        bloco2.drawRect(window.innerWidth - window.innerHeight/6  ,window.innerHeight/2 - (window.innerHeight/4)/2, window.innerHeight/6, window.innerHeight/4);
-        bloco2.endFill();
+        
+        blocoGraph2.beginFill(parseInt(lvls.levels[levelAtual].final[1],16));
+        blocoGraph2.drawRect(0, 0, window.innerHeight/6, window.innerHeight/4);
+        blocoGraph2.endFill();
+
+        bloco2 = new PIXI.Sprite(blocoGraph2.generateCanvasTexture());
+        bloco2.x = window.innerWidth - window.innerHeight/6;
+        bloco2.y = window.innerHeight/2 - window.innerHeight/8;
         tela.stage.addChild(bloco2);
     }
+ 
 }
 
 
 function iniciaCaminho(){
     //faz o bloco1 andar ate o fim do campo de jogo
     TweenMax.to(bloco1, 10, {x: window.innerWidth});
-  
+   
  }
 
  function criaMod(quant){
+    var blocoGraph = new PIXI.Graphics();
     
     //cria os modificadores conforme a quantidade existente no json
     //conforme o numero de modficadores existente, os mesmos sao posicionados de maneira correta no campo
     // ja que o campo tem tamanho dinamico
     if(quant == 2){
-        var mod1 = new PIXI.Graphics();
-        var mod2 = new PIXI.Graphics();
-        mod1.beginFill(0xff0);
-        mod1.drawRect(window.innerWidth/2 + window.innerHeight/6, window.innerHeight/2 - (window.innerHeight/6)/2, window.innerHeight/6, window.innerHeight/6);
-        mod1.endFill();
-        mod2.beginFill(0xff0);
-        mod2.drawRect(window.innerWidth/2 - window.innerHeight/6, window.innerHeight/2 - (window.innerHeight/6)/2, window.innerHeight/6, window.innerHeight/6);
-        mod2.endFill();
+        
+        blocoGraph.beginFill(0xff0);
+        blocoGraph.drawRect(0, 0, window.innerHeight/6, window.innerHeight/6);
+        blocoGraph.endFill();
+      
+        mod1 = new PIXI.Sprite(blocoGraph.generateCanvasTexture());
+        mod1.x = window.innerWidth/2 - window.innerHeight/6;
+        mod1.y = window.innerHeight/2 - window.innerHeight/12;
+        mod2 = new PIXI.Sprite(blocoGraph.generateCanvasTexture());
+        mod2.x = window.innerWidth/2 + window.innerHeight/6;
+        mod2.y = window.innerHeight/2 - window.innerHeight/12;
         tela.stage.addChild(mod1);
         tela.stage.addChild(mod2);
        }
     else{
-        var mod1 = new PIXI.Graphics();
-        mod1.beginFill(0xff0);
-        mod1.drawRect(window.innerWidth/2 - window.innerHeight/6, window.innerHeight/2 - (window.innerHeight/6)/2, window.innerHeight/6, window.innerHeight/6);
-        mod1.endFill();
+        blocoGraph.beginFill(0xff0);
+        blocoGraph.drawRect(0, 0, window.innerHeight/6, window.innerHeight/6);
+        blocoGraph.endFill()
+
+
+        mod1 = new PIXI.Sprite(blocoGraph.generateCanvasTexture());
+        mod1.x = window.innerWidth/2;
+        mod1.y = window.innerHeight/2 - window.innerHeight/12;
         tela.stage.addChild(mod1);
     }
     for(i = 0; i< quant; i++){
@@ -136,7 +173,8 @@ function iniciaCaminho(){
         modif[i] =  lvls.levels[levelAtual].modifiers[i]; 
         
     }
-   
+
+    
     
  }
 
