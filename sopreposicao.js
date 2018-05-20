1b83346196662cc4b9877d19739cb3d5f3e91c92
ticosep <ticoparronchi@gmail.com>
2018-05-20 18:55:53 -0300

    Update sopreposicao.js

--- sopreposicao.js
+++ sopreposicao.js
@@ -7,14 +7,15 @@ function sobrepos(r1, r2) {
     hit = false;
   
     
-    r1.centerX = r1.x + r1.width/2;
-    r2.bordaX = r2.x + r2.width;
+    r1.centerX = r1.x + r1.width;
+    r2.centerX = r2.x + r2.width;
    
   
   
-    //calculla a distancia entre o centro do r1 e a borda do r2
-    vx = r1.centerX - r2.bordaX;
- 
+    //calculla a distancia entre o centro do r1 e o centro do r2
+    vx = r1.centerX - r2.centerX;
+
+   
     //checa se houve sobreposicao
     if (vx >= 0) {
 
